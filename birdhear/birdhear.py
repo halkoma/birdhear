7cd1af680c2f370df7c9088eec4efa0c3c788458
halkoma <46521200+halkoma@users.noreply.github.com>
2020-03-29 17:12:06 +0300

    Get bird. Play random mp3.

--- birdhear/birdhear.py
+++ birdhear/birdhear.py
@@ -3,11 +3,20 @@
 
 import requests
 import vlc
+import time
+import random
 
 resp = requests.get('https://www.xeno-canto.org/api/2/recordings?query=troglodytes+troglodytes')
-audio_file = resp.json()['recordings'][0]['file']
-
-#p = vlc.MediaPlayer('https:' + audio_file)
-p = vlc.MediaPlayer('https://www.xeno-canto.org/sounds/uploaded/GYAUIPUVNM/XC538353-Troglodytes%20troglodytes_2020.03.16_06.03_01.mp3')
+#audio_file = resp.json()['recordings'][500]['file']
+recordings = resp.json()['recordings']
+audio_file = random.choice(recordings)['file']
+#print(len(resp.json()['recordings']))
 #print(audio_file)
+
+p = vlc.MediaPlayer('https:' + audio_file)
+
+# play the whole mp3
 p.play()
+time.sleep(1.5)
+duration = p.get_length() / 1000
+time.sleep(duration)
