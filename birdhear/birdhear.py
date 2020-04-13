f4e416dd168f5923fc76ce1c49703ad921366629
halkoma <46521200+halkoma@users.noreply.github.com>
2020-04-13 16:09:58 +0300

    Change translation page. Now it is way faster. Plus no need for Selenium.

--- birdhear/birdhear.py
+++ birdhear/birdhear.py
@@ -1,73 +1,98 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-"""Play a bird sound by name
+"""Play a bird sound by name and type (optional)
 
 Using the API https://www.xeno-canto.org/explore/api
+For the translations: https://avibase.bsc-eoc.org/
 """
 
 import random
 import re
 import sys
 import time
+from datetime import datetime
 
 import requests
+from lxml import html
 import vlc
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 
 def play_audio(audio_file):
     """Play audio of the length of the audio_file"""
     p = vlc.MediaPlayer('https:' + audio_file)
     # play the whole mp3
     p.play()
-    time.sleep(1.5)
+    time.sleep(1)
     duration = p.get_length() / 1000
     time.sleep(duration)
+
+def get_sc_name(bird_name):
+    """Search for translation of bird_name and return its scientific name"""
+
+    page = requests.get('\
+        https://avibase.bsc-eoc.org/search.jsp?pg=search&isadv=yes&startstr=\
+                        &startlang=&fam=&start=&qstr='+bird_name+'&qtype=0&\
+                        qstr2=&qtype=0&qlang=&qyears=&qauthors=&qinclsp=2')
+
+    tree = html.fromstring(page.content)
+
+    #TODO fix the problem of some birds (korppi, kaulushaikara) leading to an info page 
+    tbody = tree.xpath('//table[@class="table-striped"]')[0]
+
+    for tr in tbody[1:]:
+        if tr[-1].text == None and '[' not in tr[-2][0].text:
+            return_name = tr[-2][0].text.lower()
+    return return_name
+
+def get_audio(scientific_name, bird_type):
+    """Return random audio file to be played.
+
+    Strip away recordings < 5s
+    If type given, filter by that"""
+    payload = {'query': scientific_name}
+    resp = requests.get('https://www.xeno-canto.org/api/2/recordings',
+                        params=payload)
+    recordings = resp.json()['recordings']
+
+    if bird_type:
+        longer_recs = strip_short_recs(recordings)
+        r_by_type = [r for r in longer_recs if r['type'] == bird_type]
+        audio_file = random.choice(r_by_type)['file']
+    else:
+        longer_recs = strip_short_recs(recordings)
+        audio_file = random.choice(longer_recs)['file']
+    return audio_file
+
+def strip_short_recs(recs):
+    """Strip away recordings < 5s"""
+    time_limit = datetime.strptime('00:00:05', '%H:%M:%S').time()
+    return_recs = []
+    for rec in recs:
+        time_str = rec['length']
+        rec_length = datetime.strptime(time_str, '%M:%S').time()
+        if rec_length > time_limit:
+            return_recs.append(rec)
+        else:
+            continue
+    return return_recs
 
 def main():
     bird_name = re.sub(r"\s+", ' ', input('Bird name: ').lower().strip())
     bird_type = re.sub(r"\s+", ' ', input('Bird type: ').lower().strip())
-#    bird_name = 'harakka'
-#    bird_type = 'call'
-    payload = {'query': bird_name}
+    print()
+#    bird_name = 'punarinta'
+#    bird_type = ''
 
-    try:
-        options = Options()
-        options.headless = True
-        driver = webdriver.Firefox(options=options,
-                            executable_path=r'/usr/local/bin/geckodriver')
-        driver.get("https://www.knutas.com/birdsearch/")
-        time.sleep(0.5) # let it render
-
-        search_box = driver.find_element_by_xpath("//input")
-
-        # we loop the characters, because with "copy/paste" method 
-        # it's too fast and sometimes doesn't work. 
-        # have to wait a little bit before the last character 
-        for i in bird_name[:-1]:
-            search_box.send_keys(i)
-        time.sleep(1.5) # let it render before the last char
-        search_box.send_keys(bird_name[-1])
-        search_box.click()
-        scientific_name = driver.find_element_by_xpath(
-                                         "//td/div/sub/i").text.lower()
-        print(scientific_name)
-        driver.close()
-
-        payload = {'query': scientific_name}
-        resp = requests.get('https://www.xeno-canto.org/api/2/recordings',
-                            params=payload)
-        recordings = resp.json()['recordings']
-        if bird_type:
-            r_by_type = [r for r in recordings if r['type'] == bird_type]
-            audio_file = random.choice(r_by_type)['file']
-        else:
-            audio_file = random.choice(recordings)['file']
-        play_audio(audio_file)
-    except Exception:
-        print("Check the bird name")
-        sys.exit()
+#    try:
+    scientific_name = get_sc_name(bird_name)
+    print(bird_name, ":", scientific_name)
+    if bird_type:
+        print("type".ljust(len(bird_name)), ":", bird_type)
+    audio_file = get_audio(scientific_name, bird_type)
+    play_audio(audio_file)
+#    except Exception as e:
+#        print("Check the bird name or the type")
+#        sys.exit()
 
 if __name__== "__main__":
     main()
